bea3509a80c6404cadeb8b35ef8b910767505504
baniaf9897 <fabian9897@gmail.com>
2018-05-01 21:36:27 +0200

    create folder app/

---
+++ app/app.game.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core"
+import { METHODS } from "http";
+
+
+@Component({
+    selector: "minesweeper",
+    templateUrl: "app/app.game.html"
+})
+export class AppComponent{
+}
